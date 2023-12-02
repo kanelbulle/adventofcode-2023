dbfec32be22160e822fd6fb4b124217769d975fa
Emil Arfvidsson <emil@arfvidsson.com>
2023-12-01 22:34:19 -0800

    Add Day00 template

--- src/Day00.kt
+++ src/Day00.kt
@@ -1,72 +1,17 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        var sum = 0
-        for (line in input) {
-            val n1 = line.first { it.digitToIntOrNull() != null }
-            val n2 = line.last { it.digitToIntOrNull() != null }
-            sum += "$n1$n2".toInt()
-        }
-        return sum
+        return 0
     }
 
     fun part2(input: List<String>): Int {
-        val textDigits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-        var index = 1
-        textDigits.associateBy { index++ }
-        val allDigits = List(9) {"${it + 1}"} + textDigits
-        val digitLookup = (1..9).associateBy { textDigits[it - 1] }.toMutableMap()
-        (1..9).forEach {
-            digitLookup["$it"] = it
-        }
-
-        var sum = 0
-        for (line in input) {
-            var n1 = "" // will be either "one" or "1" etc
-            var n2 = ""
-            for (i in line.indices) {
-                val d = allDigits // map of digit -> index
-                    .associateWith { line.indexOf(it, i) }
-                    .filter { it.value > -1 }
-                    .minByOrNull { it.value }
-                if (d != null) {
-                    n1 = d.key
-                    break
-                }
-            }
-            for (i in line.indices.reversed()) {
-                val d = allDigits // map of digit -> index
-                    .associateWith { line.indexOf(it, i) }
-                    .filter { it.value > -1 }
-                    .maxByOrNull { it.value }
-                if (d != null) {
-                    n2 = d.key
-                    break
-                }
-            }
-
-            val found = "${digitLookup[n1]!!}${digitLookup[n2]!!}"
-            sum += found.toInt()
-        }
-        return sum
+        return 0
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 142)
+    val sampleInput = readInput("day02-sample")
+    val input = readInput("day02-input")
 
-    val input = readInput("day01-1-input")
-
-    part2(input.subList(0, 5)).println()
-    """
-        sdpgz3five4seven6fiveh   3 five => 35
-        876mbxbrntsfm            8 6 => 86
-        fivek5mfzrdxfbn66nine8eight five eight => 58
-        554qdg                      5 4 => 54
-        ninevsgxnine6threesix8      nine 8 => 98
-        
-        35+86+58+54+98 = 331
-
-    """.trimIndent()
-
-    part2(input).println()
+    println("part 1 sample input: ${part1(sampleInput)}")
+    println("part 1 full input: ${part1(input)}")
+    println("part 2 sample input: ${part2(sampleInput)}")
+    println("part 2 full input: ${part2(input)}")
 }
